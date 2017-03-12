cefa130bbad783f1d8897074a18e7f82ab40cd8e
Adrian Georgescu <adrian_georgescu@zumset.com>
2017-03-12 15:57:43 +0200

    Remove game id for now. Set initial scale to 1.0

--- app/App.js
+++ app/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 
 import styles from './css/App.css';
-import ApiKeys from '../ApiKeys.json';
 export default class App extends React.Component {
 
     /**
@@ -17,17 +16,17 @@ export default class App extends React.Component {
     createGame = () => {
         let gameId = this.refs.gameId.value;
         if (gameId) {
-            localStorage.setItem("gameId", gameId);
-            console.log("game id: ", gameId);
-            // window.location.href = "/score";
+            // TODO: init a new game
         }
     };
 
     render() {
         return (
-            <div>
-                <input type="text" className="form-control" placeholder="Game ID" ref="gameId" />
+            <div className="input-group">
+                <input type="text" className="form-control" placeholder="Name" ref="playerName"/>
+                <span className="input-group-btn">
                 <Link to="/score" className="btn btn-primary" onClick={this.createGame}>Enter the game</Link>
+              </span>
             </div>
         );
     }
