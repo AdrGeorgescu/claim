ee2c56db1f95a0c065b4c40dd844061ab4e507b8
Adrian Georgescu <adrian_georgescu@zumset.com>
2017-03-14 19:58:16 +0200

    Score system.

--- app/components/PlayerScoreComponent.js
+++ app/components/PlayerScoreComponent.js
@@ -11,25 +11,46 @@ export class PlayerScoreComponent extends React.Component {
         super(props);
         this.state = {
             score: 0,
-            history: []
+            history: [],
+            disabled: false
         }
     }
 
     updateScore = () => {
         let score = parseInt(this.refs.score.value);
         if (Number.isInteger(score)) {
-            this.setState({score: this.state.score + score});
+            let totalScore = this.state.score + score;
+            this.setState({score: totalScore});
+            this.checkScore(totalScore);
             let history = this.state.history;
             history.push(score);
             this.setState({history: history});
         }
     };
 
+    checkScore(score) {
+        if (score > 200) {
+            this.setState({disabled: true});
+        }
+        if (score === 200) {
+            this.setState({score: 0});
+        }
+    }
+
     undo = () => {
-        let lastValue = this.state.history.slice(-1)[0];
-        if (Number.isInteger(parseInt(lastValue))) {
+        let lastScore = this.state.history.slice(-1)[0];
+        let currentScore = this.state.score;
+
+        if (Number.isInteger(parseInt(lastScore))) {
             this.state.history.pop();
-            this.setState({score: this.state.score - lastValue});
+            let newScore = currentScore - lastScore; 
+            if (newScore < 0) {
+                newScore += 200;
+            }
+            if (newScore < 200 && this.state.disabled === true) {
+                this.setState({disabled: false});
+            }
+            this.setState({score: newScore});
         }
     }
 
@@ -41,7 +62,7 @@ export class PlayerScoreComponent extends React.Component {
                         <button className="btn btn-warning" type="button"
                                 onClick={score => this.undo()}>Undo</button>
                     </span> 
-                    <input type="number" className="form-control" ref="score" />
+                    <input type="number" className="form-control" ref="score" disabled={this.state.disabled} />
                     <span className="input-group-btn">
                         <button className="btn btn-success" type="button"
                                 onClick={score => this.updateScore()}>{this.props.player.name} - {this.state.score}</button>
