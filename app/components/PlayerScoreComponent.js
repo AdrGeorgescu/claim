9fcdd11aae9226aadc1ce30b58c3045b03d20f27
Adrian Georgescu <adrian_georgescu@zumset.com>
2017-03-14 19:08:37 +0200

    Undo functionality. In case you typed the wrong number...

--- app/components/PlayerScoreComponent.js
+++ app/components/PlayerScoreComponent.js
@@ -10,7 +10,8 @@ export class PlayerScoreComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            score: 0
+            score: 0,
+            history: []
         }
     }
 
@@ -18,18 +19,33 @@ export class PlayerScoreComponent extends React.Component {
         let score = parseInt(this.refs.score.value);
         if (Number.isInteger(score)) {
             this.setState({score: this.state.score + score});
+            let history = this.state.history;
+            history.push(score);
+            this.setState({history: history});
         }
     };
+
+    undo = () => {
+        let lastValue = this.state.history.slice(-1)[0];
+        if (Number.isInteger(parseInt(lastValue))) {
+            this.state.history.pop();
+            this.setState({score: this.state.score - lastValue});
+        }
+    }
 
     render() {
         return (
             <div key={Math.random()}>
                 <div className={styles["user-score"] + " input-group"}>
+                    <span className="input-group-btn">
+                        <button className="btn btn-warning" type="button"
+                                onClick={score => this.undo()}>Undo</button>
+                    </span> 
                     <input type="number" className="form-control" ref="score" />
                     <span className="input-group-btn">
-                    <button className="btn btn-default" type="button"
-                            onClick={score => this.updateScore()}>{this.props.player.name} - {this.state.score}</button>
-                  </span>
+                        <button className="btn btn-success" type="button"
+                                onClick={score => this.updateScore()}>{this.props.player.name} - {this.state.score}</button>
+                    </span>
                 </div>
             </div>
 
